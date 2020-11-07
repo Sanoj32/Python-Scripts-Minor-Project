5f8022cb28920052417479307b9387fff9b6e0ca
sanoj23 <sanoj.shrestha.13@gmail.com>
2020-11-07 11:34:31 +0545

    Merorojgari added

--- merorojgari.py
+++ merorojgari.py
@@ -2,10 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import json
 def merorojgari():
+    links= []
+    data = []
     source = requests.get('https://www.merorojgari.com/cat/information-technology/').text
     soup = BeautifulSoup(source, 'lxml')
-    links = soup.find_all('a', class_='title-link')
-    data = []
+    vars = soup.find_all('h2',class_='title-job')
+    for var in vars:
+        links.append(var.a['href'])
+
+
     for link in links:
         link = link['href']
         source = requests.get(link).text
@@ -14,18 +19,18 @@ def merorojgari():
         company = soup.find('a', id='job_author_name').get_text(strip=True)
         time = soup.find('div', id='job_type').get_text(strip=True)
         desct = soup.find('div', class_='description').get_text(strip=True)
-
-
+        deadline = soup.find('div',class_='date').get_text(strip=True)
+        print(deadline)
         data.append({
             'name': name,
             'company': company,
-            'deadline': time,
+            'time':time,
+            'deadline': deadline,
             'desct': desct,
             'Page_URL': link
         })
     with open('C:/Projects/itjobseeker/public/jsondata/merorojgari.json', 'w') as outfile:
         json.dump(data, outfile)
     print("merorojgari done")
-
 
 merorojgari()
--- scrape.py
+++ scrape.py
@@ -4,9 +4,11 @@ from jobsnepal import jobsnepal
 from kumarijob import kumarijob
 from linkedIn import linkedin
 from merojob import merojob
+from merorojgari import merorojgari
 
 globaljob()
 jobsnepal()
 kumarijob()
 linkedin()
+merorojgari()
 merojob()
